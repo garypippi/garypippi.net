7f0054be54e6a3f012c06a011807bbe0eba4d3e5
Naruto Ishikawa <garypippi@gmail.com>
2021-01-20 03:25:17 +0900

    Tailwindcss settings and some styles

--- modules/md/classes.ts
+++ modules/md/classes.ts
@@ -1,15 +1,15 @@
 const classes: {[key:string]: string} = {
     h1: '',
-    h2: 'my-6 text-3xl',
-    h3: '',
+    h2: 'my-6 text-2xl md:text-3xl',
+    h3: 'my-3 text-xl md:text-2xl',
     h4: '',
     h5: '',
     h6: '',
-    p: 'my-3',
+    p: 'my-3 text-sm md:text-base',
     a: '',
     img: 'my-3',
     video: 'my-3',
-    pre: 'hljs',
+    pre: 'hljs text-sm md:text-base'
 }
 
 export default Object.keys(classes).map(key => ({
