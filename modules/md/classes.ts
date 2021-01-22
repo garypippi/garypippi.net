d71febec76db5809358571ef476b00c7b3d3751b
Naruto Ishikawa <garypippi@gmail.com>
2021-01-23 00:07:25 +0900

    Add class to <code> outside of <pre>, and to <a>

--- modules/md/classes.ts
+++ modules/md/classes.ts
@@ -6,14 +6,18 @@ const classes: {[key:string]: string} = {
     h5: '',
     h6: '',
     p: 'my-3 text-sm md:text-base',
-    a: '',
+    a: 'text-red-600',
     img: 'my-3',
     video: 'my-3',
-    pre: 'hljs text-sm md:text-base'
+    pre: 'hljs my-3 text-sm md:text-base'
 }
 
 export default Object.keys(classes).map(key => ({
     type: 'output',
     regex: new RegExp(`<${key}(?: (.*)>|>)`, 'g'),
     replace: `<${key} class="${classes[key]}" $1>`
-}))
+})).concat({
+    type: 'output',
+    regex: /(?<!(?:<pre(?: (.*)>|>)))(?:\n?\s*<code(?: (.*)>|>))/g,
+    replace: '<code class="bg-gray-100 px-2 py-px mx-px" $1>'
+})
